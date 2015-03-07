ed8253c1d322be25e8cf56228f613ccc4ce75484
Xofii <sofiabermejosantos@gmail.com>
2015-03-07 19:45:41 +0100

    Ventana de colores

---
+++ Aprendo con Molly/colores.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aprendo_con_Molly
+{
+	/// <summary>
+	/// Lógica de interacción para colores.xaml
+	/// </summary>
+	public partial class colores : Window
+	{
+		public colores()
+		{
+			this.InitializeComponent();
+			
+			// A partir de este punto se requiere la inserción de código para la creación del objeto.
+		}
+	}
+}
